045e46359e501e3b7c04ab2f571a3f271f9bffce
WU Yijie <me@wuyj.net>
2018-03-16 18:00:21 +0800

    cookie support

--- utils.js
+++ utils.js
@@ -16,6 +16,28 @@ function decrypt(encryptedMsg, passphrase){
     var ret = CryptoJS.AES.decrypt(encryptedHTML, passphrase).toString(CryptoJS.enc.Utf8);
     return ret;
 }
+
+function setCookie(cname, cvalue, exhrs) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exhrs*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 function get_json(url, handle){
 	$.ajax({
 		url: url,
