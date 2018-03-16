48e414c1415a212cdec9921820f05a92f1e5de16
WU Yijie <me@wuyj.net>
2018-03-16 18:42:09 +0800

    canvas drawing

--- utils.js
+++ utils.js
@@ -61,7 +61,7 @@ for(var i in prefill){
 function submit_one(formId, ID,TYPE,DATA,STAMP) {
 	
 	var posturl = 'https://docs.google.com/forms/d/e/' + formId + '/formResponse';
-  var xhr = new XMLHttpRequest();
+	var xhr = new XMLHttpRequest();
   
   xhr.onreadystatechange = function() {
     if (this.readyState == XMLHttpRequest.DONE){
@@ -85,4 +85,92 @@ function submit_one(formId, ID,TYPE,DATA,STAMP) {
     fd.append(item, data[item]);
   }
   xhr.send(fd);
+}
+
+function register(el, evts){
+	for(var type in evts){
+	  el.addEventListener(type, evts[type]);
+	}
+}
+
+function selector(){
+	var pressed = false;
+	var boundary = [];
+	var boxP1 = {};
+	var boxP2 = {};
+	var lastPos = {};
+
+  function updateBoundary(x,y){
+    var dx = x - lastPos.x;
+    var dy = y - lastPos.y;
+    if(dx == 0 && dy == 0){
+      return;
+    }
+
+    // connect
+    if(dx*dx > 1 && dy*dy<=dx*dx){
+      var step = dx>0? 1: -1;
+      var r = dy/dx;
+
+      for(var j=1;j<dx/step;++j){
+        boundary.push({x:lastPos.x+step*j, y:lastPos.y+Math.floor(step*j*r)});
+      }
+    }else if(dy*dy > 1 && dy*dy>=dx*dx){
+      var step = dy>0? 1: -1;
+      var r = dx/dy;
+      for(var j=1;j<dy/step;++j){
+        boundary.push({x:lastPos.x+Math.floor(step*j*r), y:lastPos.y+step*j});
+      }
+    }
+    lastPos = {x:x, y:y};
+    boundary.push(lastPos);
+
+    // update bounding box
+    if(x < boxP1.x){
+      boxP1.x = x;
+    }else if(x > boxP2.x){
+      boxP2.x = x;
+    }
+    if(y < boxP1.y){
+      boxP1.y = y;
+    }else if(y > boxP2.y){
+      boxP2.y = y;
+    }
+
+  }
+
+  function pressing(e){
+    if(pressed){
+      return false;
+    }
+    boundary = [];
+    lastPos = {x:e.offsetX, y:e.offsetY};
+    boundary.push(lastPos);
+    boxP1 = {x:e.offsetX, y:e.offsetY};
+    boxP2 = {x:e.offsetX, y:e.offsetY};
+    pressed = true;
+    return true;
+  }
+
+  function release(e){
+    if(!pressed){
+      return false;
+    }
+    //updateBoundary(boundary[0].x, boundary[0].y);
+    pressed = false;
+
+    return {
+      boundary: boundary,
+      boxP1: boxP1,
+      boxP2: boxP2
+    }
+
+  }
+  return {
+    pressing: pressing,
+    pressed: function(){return pressed},
+    release: release,
+    updateBoundary: updateBoundary
+  }
+
 }
